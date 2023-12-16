f501533ee8058286c8d55185de957a3f15ae68e4
kana <k.salon.du.l@gmail.com>
2023-12-16 20:57:26 +0900

    一般予約処理完了

--- src/pages/reservation.tsx
+++ src/pages/reservation.tsx
@@ -1,117 +1,148 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { createBooking } from "@/utils/api";
+import { getRoomCapacity, createBooking } from "@/utils/api";
+import { push } from "firebase/database";
 
 const ReservationPage: React.FC = () => {
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [participants, setParticipants] = useState(0);
   const router = useRouter();
-  const { roomId } = router.query;
-  const [guestName, setGuestName] = useState(""); // ゲストユーザーの名前
-  const [guestEmail, setGuestEmail] = useState(""); // ゲストユーザーのメールアドレス
+  const [roomId, setRoomId] = useState<number | null>(null);
+  const [roomCapacity, setRoomCapacity] = useState<number | null>(null);
+  const [reservationDetails, setReservationDetails] = useState({
+    startDateTime: "",
+    endDateTime: "",
+    mainUserEmployeeNumber: "",
+    memberEmployeeNumbers: "",
+    guestNames: "",
+    // その他の必要な情報をここに追加
+  });
 
-  const generateTemporaryUserId = () => {
-    return Math.floor(Math.random() * 1000000);
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    // roomIdがない場合はエラーを投げる
-    if (!roomId) {
-      console.error("Room ID is not specified.");
-      // 必要に応じてユーザーにエラーメッセージを表示する
-      return;
-    }
-
-    const tempUserId = generateTemporaryUserId();
-
-    const reservationData = {
-      user_id: tempUserId,
-      room_id: Array.isArray(roomId) ? roomId[0] : roomId,
-      start_datetime: startDate,
-      end_datetime: endDate,
-      booked_num: participants,
-      guest_user: {
-        name: guestName,
-        email: guestEmail,
-      },
+  useEffect(() => {
+    const fetchRoomDetails = async (id: number) => {
+      try {
+        const capacity = await getRoomCapacity(id);
+        setRoomCapacity(capacity);
+      } catch (error) {
+        console.error("Failed to fetch room details:", error);
+      }
     };
 
-    console.log("Sending reservation data:", reservationData);
+    if (router.query.roomId) {
+      const id = Number(router.query.roomId);
+      setRoomId(id);
+      fetchRoomDetails(id);
+    }
+  }, [router.query]);
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { id, value } = e.target;
+    setReservationDetails({ ...reservationDetails, [id]: value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
     try {
-      // 予約データを送信
-      const result = await createBooking(reservationData);
-      console.log("Booking successful:", result);
-      // 送信後の処理（例: 予約完了ページへのリダイレクト）
+      // 社員番号から user_id を取得するロジック（適切なAPIエンドポイントを使用）
+      const userIdResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/employee_number/${reservationDetails.mainUserEmployeeNumber}`
+      );
+      if (!userIdResponse.ok) {
+        throw new Error("Failed to retrieve user information.");
+      }
+      const userIdData = await userIdResponse.json();
+
+      // 予約データの構築（user_id を含める）
+      const bookingData = {
+        user_id: userIdData.user_id, // 取得した user_id を使用
+        room_id: roomId,
+        start_datetime: reservationDetails.startDateTime,
+        end_datetime: reservationDetails.endDateTime,
+        main_user_employee_number: reservationDetails.mainUserEmployeeNumber,
+        member_employee_numbers: reservationDetails.memberEmployeeNumbers
+          .split(",")
+          .map((s) => s.trim())
+          .filter((s) => s),
+        guest_names: reservationDetails.guestNames
+          .split(",")
+          .map((s) => s.trim())
+          .filter((s) => s),
+      };
+
+      // 予約データの送信
+      const result = await createBooking(bookingData);
+      console.log("予約成功:", result);
+      alert("予約完了");
+      // 成功時の処理
     } catch (error) {
-      console.error("Booking failed:", error);
-      // エラーハンドリング（例: ユーザーへのエラーメッセージ表示）
+      console.error("予約エラー:", error);
+      alert(
+        "エラー: 役員専用のルームのため、主要利用者の社員番号を役員のIDにしてください。"
+      );
     }
   };
 
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-gray-900 text-center my-4">会議室予約</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="guestName">ゲスト名:</label>
-            <input
-              type="text"
-              id="guestName"
-              value={guestName}
-              onChange={(e) => setGuestName(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="guestEmail">ゲストのメール:</label>
-            <input
-              type="email"
-              id="guestEmail"
-              value={guestEmail}
-              onChange={(e) => setGuestEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="startDate">開始日時:</label>
-            <input
-              type="datetime-local"
-              id="startDate"
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="endDate">終了日時:</label>
-            <input
-              type="datetime-local"
-              id="endDate"
-              value={endDate}
-              onChange={(e) => setEndDate(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="participants">参加人数:</label>
-            <input
-              type="number"
-              id="participants"
-              value={participants}
-              onChange={(e) => setParticipants(parseInt(e.target.value))}
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              予約する
-            </button>
-          </div>
-        </form>
-      </div>
+    <div>
+      <h1>予約フォーム</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="roomId">部屋番号</label>
+          <input type="text" id="roomId" value={roomId || ""} readOnly />
+        </div>
+        <div>
+          <label>部屋のキャパシティ</label>
+          <span>{roomCapacity !== null ? roomCapacity : "Loading..."}</span>
+        </div>
+        <div>
+          <label htmlFor="startDateTime">開始日時</label>
+          <input
+            type="datetime-local"
+            id="startDateTime"
+            value={reservationDetails.startDateTime}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="endDateTime">終了日時</label>
+          <input
+            type="datetime-local"
+            id="endDateTime"
+            value={reservationDetails.endDateTime}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="mainUserEmployeeNumber">主要利用者の社員番号</label>
+          <input
+            type="text"
+            id="mainUserEmployeeNumber"
+            value={reservationDetails.mainUserEmployeeNumber}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="memberEmployeeNumbers">
+            参加する社員の社員番号 (カンマ区切り)
+          </label>
+          <textarea
+            id="memberEmployeeNumbers"
+            value={reservationDetails.memberEmployeeNumbers}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="guestNames">ゲストの名前 (カンマ区切り)</label>
+          <textarea
+            id="guestNames"
+            value={reservationDetails.guestNames}
+            onChange={handleInputChange}
+          />
+        </div>
+        {/* その他の入力フィールド */}
+        <button type="submit">予約する</button>
+      </form>
     </div>
   );
 };
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -6,6 +6,29 @@ interface APIOptions {
   headers?: HeadersInit;
 }
 
+interface ValidationError {
+  message: string;
+  // 他の必要なフィールドがあればここに追加
+}
+
+// エラーレスポンスボディの型を定義
+interface ErrorResponse {
+  detail: ValidationError[];
+}
+
+interface ValidationErrorDetail {
+  loc: string[];
+  msg: string;
+  type: string;
+}
+
+interface DetailError {
+  type: string;
+  loc: string[];
+  msg: string;
+  // その他のプロパティがあればここに追加
+}
+
 export async function fetchAPI(
   endpoint: string,
   { method = "GET", body = null, headers = new Headers() }: APIOptions = {}
@@ -24,13 +47,30 @@ export async function fetchAPI(
 
   const options: RequestInit = { method, headers, body };
 
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    const errorBody = await response.json();
-    console.error("Error response body:", errorBody);
-    throw new Error(`Failed to fetch ${url}: ${response.status}`);
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      const errorBody = await response.json();
+      console.error("Error response body:", errorBody);
+
+      if (errorBody.detail && Array.isArray(errorBody.detail)) {
+        errorBody.detail.forEach((detailError: DetailError) => {
+          console.error("Validation error details:", detailError);
+          if (detailError.loc) {
+            console.error(
+              "Missing or invalid field:",
+              detailError.loc.join(" > ")
+            );
+          }
+        });
+      }
+      throw new Error(`Failed to fetch ${url}: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Error in fetchAPI:", error);
+    throw error;
   }
-  return response.json();
 }
 
 export const getRooms = (): Promise<any[]> => fetchAPI("/rooms");
@@ -57,3 +97,27 @@ export const updateBooking = (bookingId: string, data: any): Promise<any> =>
   fetchAPI(`/bookings/${bookingId}`, { method: "PUT", body: data });
 export const deleteBooking = (bookingId: string): Promise<any> =>
   fetchAPI(`/bookings/${bookingId}`, { method: "DELETE" });
+
+export const getRoomCapacity = async (roomId: number): Promise<number> => {
+  try {
+    const room = await fetchAPI(`/rooms/${roomId}`);
+    return room.capacity || 0;
+  } catch (error) {
+    console.error("Failed to fetch room capacity:", error);
+    return 0;
+  }
+};
+
+export async function getUserIdByEmployeeNumber(employeeNumber: string) {
+  try {
+    const response = await fetchAPI(`/users/employee_number/${employeeNumber}`);
+    if (!response.ok) {
+      throw new Error("Failed to retrieve user information.");
+    }
+    const userInfo = await response.json();
+    return userInfo.user_id;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error; // エラーを再スローするか、適切な方法でハンドリング
+  }
+}
